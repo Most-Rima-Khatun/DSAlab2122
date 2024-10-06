468c51e783a3a6d571bf84deb7f590fb0cba3f14
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2024-10-06 16:45:46 +0600

    Add files via upload
    shortest path ,strongly connected or not ,adjacency matrix representation

---
+++ Graph_strongly_connected.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 99999;
+
+void warshall(int graph[100][100],int n){
+    int temp[100][100];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            temp[i][j] = graph[i][j];
+        }
+    }
+    for(int k = 0; k < n ; k++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n ; j++)
+            {
+                temp[i][j] = min(temp[i][j],(temp[i][k]+temp[k][j]));
+            }
+        }
+    }
+    cout<<"the sortest path:"<<"\n\n";
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            if(temp[i][j] == 99999)
+            {
+                cout<<"INF"<<" ";
+            }else
+            {
+                cout<<temp[i][j]<<" ";
+            }
+        }cout<<"\n";
+    }
+    cout<<"the path matrix:"<<"\n\n";
+    for(int k = 0; k < n ; k++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n ; j++)
+            {
+                temp[i][j] = temp[i][j]||(temp[i][k]&&temp[k][j]);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            cout<<temp[i][j]<<" ";
+        }cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter the number of nodes:"<<"\n\n";
+    cin>>n;
+    cout<<n<<endl;
+    int graph[100][100];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            cin>>graph[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            if(graph[i][j] == 0)
+            {
+                graph[i][j] = 99999;
+            }
+        }
+    }
+    cout<<"the adjacency matrix is :"<<"\n\n";
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            cout<<graph[i][j]<<" ";
+        }cout<<"\n";
+    }
+    warshall(graph,n);
+    
+    return 0;
+}
